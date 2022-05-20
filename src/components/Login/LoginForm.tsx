537a4b06496e48f2c8839de30ad5bcdf3b91e0f8
Gustavo Augusto <gacl@ic.ufal.br>
2022-05-19 22:48:48 -0300

    Add validations and toast messages

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,18 +1,43 @@
 import React, { useState } from "react"
 import axios, { AxiosError } from 'axios'
+import * as Yup from 'yup';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
+import getValidationErrors from '../../utils/getValidationErrors'
 
 import { useAuthContext } from "../../hooks/useAuth"
 import { api } from "../../services/api"
 
-export function LoginForm() {
+export default function LoginForm() {
     const { login } = useAuthContext()
     const [email, setEmail] = useState('')
+    const [emailValidation, setEmailValidation] = useState('')
     const [password, setPassword] = useState('')
+    const [passwordValidation, setPasswordValidation] = useState('')
     
     async function handleLogin(event: React.FormEvent) {
         event.preventDefault()
 
         try {
+          setEmailValidation('')
+          setPasswordValidation('')
+
+          const schema = Yup.object().shape({
+            email: Yup.string()
+              .email('Digite um email válido')
+              .required('E-mail obrigatório'),
+            password: Yup.string().required('Senha obrigatória'),
+          });
+
+          const data = {
+            email,
+            password
+          }
+
+          await schema.validate(data, {
+            abortEarly: false,
+          });
+
           const response = await api.post('/auth/login', {
             email,
             password,
@@ -20,17 +45,44 @@ export function LoginForm() {
 
           const JWT = response.data.data
           login(JWT)
-        } catch (err: any | AxiosError) {
+
+          toast.success('login realizado com sucesso. Você será redirecionado para a página principal', {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 2500
+          });
+        } catch (err: any | AxiosError | Yup.ValidationError) {
           if (axios.isAxiosError(err)) {
             console.log(err.message)
           } else {
             console.log(err)
+            
+            if (err instanceof Yup.ValidationError) {
+              const errorsFromYup = getValidationErrors(err);
+              
+              toast.error('Erro ao realizar login, por favor tente novamente.', {
+                position: toast.POSITION.TOP_RIGHT,
+                autoClose: 2500
+              });
+              
+              Object.entries(errorsFromYup).forEach(([key, value]: any) => {      
+                if(key === 'email') {
+                  console.log("AQUI")
+                  setEmailValidation(value);
+                } else if(key === 'password') {
+                  setPasswordValidation(value);
+                }
+               });
+              
+              return;
+            }
+
           }
         }
     }
 
     return (
         <form className="flex flex-col">
+                {emailValidation && <label className="text-[#F93633]">* {emailValidation}</label>}
                 <input 
                   type="email"
                   placeholder="Email"
@@ -38,12 +90,13 @@ export function LoginForm() {
                   className="w-[421px] mb-8 px-4 py-[22px] text-[#49454F] text-2xl leading-6 placeholder:text-[#49454F] border border-black border-opacity-60 rounded"
                 />
 
+                {passwordValidation && <label className="text-[#F93633]">* {passwordValidation}</label>}
                 <input 
                   type="password"
                   placeholder="Senha"
-                  onChange={(e) => setPassword(e.target.value)}
+                  onChange={(e) => setPassword(e.target.value)}        
                   className="w-[421px] mb-5 px-4 py-[22px] text-[#49454F] text-2xl leading-6 placeholder:text-[#49454F] border border-black border-opacity-60 rounded"
-                />
+                  />
 
                 <a 
                   href="/"

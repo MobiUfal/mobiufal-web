f24899bdb0c5a388428daa7206a8e9c05d06928e
Elygledson Js <47836992+Elygledson@users.noreply.github.com>
2022-11-30 10:20:20 +0000

    Pin_sidebar_and_navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,7 +10,7 @@ interface HeaderProps {
 export function Header({ isSidebarOpened, openSidebar }: HeaderProps) {
   return (
     <>
-      <header className="z-10 relative flex w-full h-[65px] pr-9 pl-7 items-center justify-between bg-[#29AAD7] rounded-b-xl">
+      <header className="fixed top-0 z-10 flex w-full h-[65px] pr-9 pl-7 items-center justify-between bg-[#29AAD7] rounded-b-xl">
         <div className="flex items-center">
           <SidebarButton
             isSidebarOpened={isSidebarOpened}
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,6 @@
 import { BsHouseDoorFill } from "react-icons/bs";
 import { FaUserCircle } from "react-icons/fa";
 import { IoSettingsSharp } from "react-icons/io5";
-
 import { SectionPage } from "./SectionPage";
 import { SubsectionPage } from "./SubsectionPage";
 
@@ -12,9 +11,9 @@ interface SidebarProps {
 export function Sidebar({ isSidebarOpened }: SidebarProps) {
   return (
     <aside
-      className={`overflow-hidden z-1 py-[calc(2.25rem+12px)] translate-y-[-12px] h-[calc(100vh-6rem+52px)]    flex flex-col bg-[#373737] rounded-br-xl ease-in-out duration-300 ${
+      className={`fixed  overflow-hidden  z-[1] py-[110px] translate-y-[-12px] h-[100%] bg-[#373737] rounded-br-xl ease-in-out duration-300 ${
         isSidebarOpened ? "w-[260px]" : "w-0"
-      }`}
+      }`}  
     >
       <nav className="flex flex-col gap-y-3.5">
         <SectionPage

4fa78319874a8550a63a1a27bf901205df4785ac
Elygledson Js <47836992+Elygledson@users.noreply.github.com>
2023-03-21 21:52:54 +0000

    Feat/ add pagination and new table

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
